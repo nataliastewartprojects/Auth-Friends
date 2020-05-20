6eabc5cd872085bba917d5abb80c0c5b8099f11a
Natalia <nataliamirandac@hotmail.com>
2020-05-19 22:07:01 -0400

    Displayed the list of friends on the browser - protected page- FriendsList

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -18,15 +18,27 @@ class FriendsList extends React.Component {
       .get("/api/friends")
       .then((res) => {
         console.log("FRIENDSLIST RES:", res);
-        // this.setState({
-        //   friends: res.data,
-        // });
+        this.setState({
+          friends: res.data,
+        });
       })
       .catch((err) => console.log("FRIENDSLIST- ERROR", err));
   };
 
   render() {
-    return <div className="friends-container">Hi from FriendsList</div>;
+    return (
+      <div className="friends-container">
+        {this.state.friends.map((item) => {
+          return (
+            <div key={item.id}>
+              <h3>{item.name}</h3>
+              <p>Age: {item.age}</p>
+              <p>Email:{item.email}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
